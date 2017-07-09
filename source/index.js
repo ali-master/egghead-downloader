4817b430803de541e938186545d1e5e7063d539a
Ali Torki <torki@nazarbazaar.ir>
2017-07-10 01:09:27 +0430

    clean: index.js

--- source/index.js
+++ source/index.js
@@ -1,7 +1,3 @@
 import "babel-polyfill";
 
 import CheckURL from './utils/CheckURL';
-
-const checking = new CheckURL('https://egghead.com');
-
-console.log('url', checking.URL, "version", VERSION);
